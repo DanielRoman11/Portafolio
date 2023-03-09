26019be77412a03debeaa5a837ff665268f3c5c3
danielroman11 <danielroman.05111@gmail.com>
2023-03-09 02:06:47 -0500

    90% Completado

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const principal = document.getElementById("principal");
 const seccion_2 = document.getElementById("seccionDos");
+const curso = document.getElementById("curso");
 
 const estilo = window.getComputedStyle(seccion_2, null);
+const estiloCurso = window.getComputedStyle(curso, null);
 
 function mostrarSeccion(){
   if (estilo.display === "none" && screen.width >= 600){
@@ -16,4 +18,10 @@ function mostrarSeccion(){
     seccion_2.style.display = 'none'
     return principal.style.gridTemplateColumns = 'repeat(1, minmax(0, 1fr))'
   }
+}
+function mostrarCursos(){
+  if(estiloCurso.display === "none" && screen.width >= 600){
+    return curso.style.display == 'flex'
+  }
+  return curso.style.display == 'none'
 }
